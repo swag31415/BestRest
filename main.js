118915d432728368b9558f191052f7ced6b376e0
swag31415 <kidswagatam18@gmail.com>
2023-06-05 20:31:29 -0400

    Added Firebase Login

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
+// Import the required Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA01r55OhBoD_-lMSGTcTNny5jWHqroqkE",
+  authDomain: "bestagon-4eda5.firebaseapp.com",
+  projectId: "bestagon-4eda5",
+  storageBucket: "bestagon-4eda5.appspot.com",
+  messagingSenderId: "576359900133",
+  appId: "1:576359900133:web:6308a82bcde5da1a7b850c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 new Vue({
   el: '#app',
   data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      loginError: ''
     }
   },
   methods: {
     signIn() {
-      // Perform sign-in logic here
-      console.log('Email:', this.email);
-      console.log('Password:', this.password);
+      const auth = getAuth();
+
+      signInWithEmailAndPassword(auth, this.email, this.password)
+        .then((userCredential) => {
+          // Login successful, do something here (e.g., redirect to another page)
+          console.log('User:', userCredential.user);
+        })
+        .catch((error) => {
+          // Login failed, display error message
+          this.loginError = error.message;
+        });
     }
   }
 });
